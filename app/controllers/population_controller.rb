1da447902ee7b395bf8209a18cdc97a376197bbb
sziszeia <sziszeia@gmail.com>
2019-11-23 14:50:37 +0000

    Added compariosn form and chart

--- app/controllers/population_controller.rb
+++ app/controllers/population_controller.rb
@@ -21,37 +21,67 @@ class PopulationController < ApplicationController
             end
 
             # Populate @country with GBR as default or by passed in params
-            if params[:SelectCountry].blank?
-                @country = @countries.where(countryCode: 'GBR').take
+            if params[:SelectCountry1].blank?
+                @country1 = @countries.where(countryCode: 'GBR').take
             else
-                @country = @countries.where(countryCode: params[:SelectCountry]).take
+                @country1 = @countries.where(countryCode: params[:SelectCountry1]).take
+            end
+
+            if params[:SelectCountry2].blank?
+                @country2 = @countries.where(countryCode: 'GBR').take
+            else
+                @country2 = @countries.where(countryCode: params[:SelectCountry2]).take
             end
 
             # Once @country is known we need the associated population data and indicator
-            @population = @populations.where(countryCode: @country.countryCode).take
-            @indicator = @indicators.where(indicator_code: @population.indicatorCode).take
+            @population1 = @populations.where(countryCode: @country1.countryCode).take
+            @indicator1 = @indicators.where(indicator_code: @population1.indicatorCode).take
 
-            # Get GDP per year for the selected country
-            @annual_population = []
-            @annual_population.push(@population.Y2000)
-            @annual_population.push(@population.Y2001)
-            @annual_population.push(@population.Y2002)
-            @annual_population.push(@population.Y2003)
-            @annual_population.push(@population.Y2004)
-            @annual_population.push(@population.Y2005)
-            @annual_population.push(@population.Y2006)
-            @annual_population.push(@population.Y2007)
-            @annual_population.push(@population.Y2008)
-            @annual_population.push(@population.Y2009)
-            @annual_population.push(@population.Y2010)
-            @annual_population.push(@population.Y2011)
-            @annual_population.push(@population.Y2012)
-            @annual_population.push(@population.Y2013)
-            @annual_population.push(@population.Y2014)
-            @annual_population.push(@population.Y2015)
-            @annual_population.push(@population.Y2016)
-            @annual_population.push(@population.Y2017)
-            @annual_population.push(@population.Y2018)
+            @population2 = @populations.where(countryCode: @country2.countryCode).take
+            @indicator2 = @indicators.where(indicator_code: @population2.indicatorCode).take
+
+            # Get population per year for the selected country
+            @annual_population1 = []
+            @annual_population1.push(@population1.Y2000)
+            @annual_population1.push(@population1.Y2001)
+            @annual_population1.push(@population1.Y2002)
+            @annual_population1.push(@population1.Y2003)
+            @annual_population1.push(@population1.Y2004)
+            @annual_population1.push(@population1.Y2005)
+            @annual_population1.push(@population1.Y2006)
+            @annual_population1.push(@population1.Y2007)
+            @annual_population1.push(@population1.Y2008)
+            @annual_population1.push(@population1.Y2009)
+            @annual_population1.push(@population1.Y2010)
+            @annual_population1.push(@population1.Y2011)
+            @annual_population1.push(@population1.Y2012)
+            @annual_population1.push(@population1.Y2013)
+            @annual_population1.push(@population1.Y2014)
+            @annual_population1.push(@population1.Y2015)
+            @annual_population1.push(@population1.Y2016)
+            @annual_population1.push(@population1.Y2017)
+            @annual_population1.push(@population1.Y2018)
+
+            @annual_population2 = []
+            @annual_population2.push(@population2.Y2000)
+            @annual_population2.push(@population2.Y2001)
+            @annual_population2.push(@population2.Y2002)
+            @annual_population2.push(@population2.Y2003)
+            @annual_population2.push(@population2.Y2004)
+            @annual_population2.push(@population2.Y2005)
+            @annual_population2.push(@population2.Y2006)
+            @annual_population2.push(@population2.Y2007)
+            @annual_population2.push(@population2.Y2008)
+            @annual_population2.push(@population2.Y2009)
+            @annual_population2.push(@population2.Y2010)
+            @annual_population2.push(@population2.Y2011)
+            @annual_population2.push(@population2.Y2012)
+            @annual_population2.push(@population2.Y2013)
+            @annual_population2.push(@population2.Y2014)
+            @annual_population2.push(@population2.Y2015)
+            @annual_population2.push(@population2.Y2016)
+            @annual_population2.push(@population2.Y2017)
+            @annual_population2.push(@population2.Y2018)
 
     end
 end
