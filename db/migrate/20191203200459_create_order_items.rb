fa6ec8fa539116641ce10e9578a891e8f6c96c36
sziszeia <sziszeia@gmail.com>
2019-12-03 22:43:32 +0000

    Heroku migration fix

--- db/migrate/20191203200459_create_order_items.rb
+++ db/migrate/20191203200459_create_order_items.rb
@@ -1,10 +1,13 @@
 class CreateOrderItems < ActiveRecord::Migration[5.2]
   def change
     create_table :order_items do |t|
-      t.string :order_id
-      t.string :integer
-      t.string :product_id
-      t.string :integer
+      # t.string :order_id
+      # t.string :integer
+      # t.string :product_id
+      # t.string :integer
+
+      t.integer :order_id
+      t.integer :product_id
 
       t.timestamps
     end
